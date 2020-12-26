de6b92c8d7aecc2faab976de3398d7f92bde2406
Gonçalo Oliveira <goncalo.oli@outlook.com>
2020-12-26 21:17:01 +0000

    Components now display the user's name and profile picture

--- client/src/components/PostsForm.jsx
+++ client/src/components/PostsForm.jsx
@@ -1,4 +1,5 @@
 import api from "../api/axiosAPI";
+import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
@@ -8,14 +9,18 @@ const PostsForm = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    api.post("/posts", {
-      title: e.target[0].value,
-      text: e.target[1].value,
-    }).then(res => props.setPosts(old => old.concat([res.data]))).catch(console.error);
+    api.get("/profile/username").then(res => {
+      api.post("/posts", {
+        title: e.target[0].value,
+        text: e.target[1].value,
+        author: res.data
+      }).then(res => props.setPosts(old => old.concat([res.data]))).catch(console.error)
+    }).catch(console.error);
   };
 
   return (
     <Form className="posts-form" onSubmit={handleSubmit}>
+      <Link to="/profile">View Profile</Link>
       <Form.Group controlId="formTitle">
         <Form.Label>Title</Form.Label>
         <Form.Control as="textarea" rows={3} placeholder="Enter post title" />
--- client/src/components/PostsList.jsx
+++ client/src/components/PostsList.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import api from "../api/axiosAPI";
 import Card from "react-bootstrap/Card";
-import img from "../assets/logo.svg";
 
 import "./PostsList.css";
 
@@ -16,10 +15,11 @@ const PostsList = (props) => {
   return (
     props.posts.map((post, index) => (
         <Card style={{ width: '20rem' }} key={index}>
-          <Card.Img variant="top" src={img} />
+          <Card.Img variant="top" src={api.getBaseUrl() + post.authorProfilePic} />
           <Card.Body>
             <Card.Title>{post.title}</Card.Title>
             <Card.Text>{post.text}</Card.Text>
+            <small className="text-muted">Posted by {post.author}</small>
           </Card.Body>
         </Card>
       ))
