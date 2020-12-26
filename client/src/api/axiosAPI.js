1150bf63be22f04e325b13bea67fb215527e82d0
Gonçalo Oliveira <goncalo.oli@outlook.com>
2020-12-26 21:16:17 +0000

    Added getBaseUrl to the api

--- client/src/api/axiosAPI.js
+++ client/src/api/axiosAPI.js
@@ -1,10 +1,11 @@
 import axios from "axios"
 
-let base_url = "http://localhost:3001";
+let baseUrl = "http://localhost:3001";
 
 let api = {
-    get: (endpoint, config) => axios.get(base_url + endpoint, config),
-    post: (endpoint, values, config) => axios.post(base_url + endpoint, values, config)
+    get: (endpoint, config) => axios.get(baseUrl + endpoint, config),
+    post: (endpoint, values, config) => axios.post(baseUrl + endpoint, values, config),
+    getBaseUrl: () => baseUrl
 };
 
 export default api;
